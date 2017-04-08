6ad4e9ed6aed5b69c7f387c8289538bc1742ddc3
Jeremy Wall <jeremy.wall@gmail.com>
2017-04-07 21:14:26 -0700

    add to and from fields

--- routes/polly.js
+++ routes/polly.js
@@ -15,9 +15,9 @@ router.post('/test', function (req, res, next) {
 
     var params = {
         OutputFormat: 'mp3',
-        Text: req.body.text, //'<speak>Hey there Brian, I just love that push you made to Git Hub. Talk with you later <break time="250ms"/>  tootles!</speak>', //req.body.text,
+        Text: req.body.message, //'<speak>Hey there Brian, I just love that push you made to Git Hub. Talk with you later <break time="250ms"/>  tootles!</speak>', //req.body.text,
         VoiceId: 'Salli',
-        TextType: (req.body.text.indexOf('<speak>') !== -1) ? 'ssml' : 'text'
+        TextType: (req.body.message.indexOf('<speak>') !== -1) ? 'ssml' : 'text'
     };
 
     polly.synthesizeSpeech(params, function (err, data) {
@@ -64,8 +64,8 @@ router.post('/test', function (req, res, next) {
                             console.log('putObject xml ok');
                             twilio.calls.create({
                                 url: 'https://s3.amazonaws.com/' + bucketName + '/' + ts + '.xml',
-                                to: '+',
-                                from: '+',
+                                to: '+' + req.body.to,
+                                from: '+' + req.body.from,
                                 method: 'GET'
                             }, function (err, call) {
                                 if (err) {
