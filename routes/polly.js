32f363a6f4a59196d97fbdaa8dbb1450956c664a
Jeremy Wall <jeremywall@users.noreply.github.com>
2017-05-11 10:34:31 -0700

    Update polly.js

--- routes/polly.js
+++ routes/polly.js
@@ -11,76 +11,77 @@ router.post('/test', function (req, res, next) {
 
     if (req.body.password !== process.env.PASSWORD) {
         res.json({status: 'bad password'});
+    } else {
+
+        var params = {
+            OutputFormat: 'mp3',
+            Text: req.body.message, //'<speak>Hey there Brian, I just love that push you made to Git Hub. Talk with you later <break time="250ms"/>  tootles!</speak>', //req.body.text,
+            VoiceId: 'Salli',
+            TextType: (req.body.message.indexOf('<speak>') !== -1) ? 'ssml' : 'text'
+        };
+
+        polly.synthesizeSpeech(params, function (err, data) {
+            if (err) {
+                console.log('synthesizeSpeech err', err);
+                res.json(err);
+            } else {
+                console.log('synthesizeSpeech ok');
+                var ts = (new Date).getTime();
+                var bucketName = process.env.BUCKETNAME;
+                var s3Params = {
+                    Bucket: bucketName,
+                    Key: ts + '.mp3',
+                    ACL: 'public-read',
+                    Body: data.AudioStream,
+                    ContentType: 'audio/mpeg',
+                    Expires: (ts / 1000) + (120)
+                };
+                s3.putObject(s3Params, function (err, data) {
+                    if (err) {
+                        console.log('putObject mp3 err');
+                        res.json(err);
+                    } else {
+                        console.log('putObject mp3 ok');
+    
+                        s3Params = {
+                            Bucket: bucketName,
+                            Key: ts + '.xml',
+                            ACL: 'public-read',
+                            Body: '<?xml version="1.0" encoding="UTF-8"?>' +
+                            '<Response>' +
+                            '<Play>' + 'https://s3.amazonaws.com/' + bucketName + '/' + ts + '.mp3' + '</Play>' +
+                            '</Response>'
+                            ,
+                            ContentType: 'text/xml',
+                            Expires: (ts / 1000) + (120)
+                        };
+
+                        s3.putObject(s3Params, function (err, data) {
+                            if (err) {
+                                console.log('putObject xml err');
+                                res.json(err);
+                            } else {
+                                console.log('putObject xml ok');
+                                twilio.calls.create({
+                                    url: 'https://s3.amazonaws.com/' + bucketName + '/' + ts + '.xml',
+                                    to: req.body.to,
+                                    from: req.body.from,
+                                    method: 'GET'
+                                }, function (err, call) {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        console.log(call.sid);
+                                    }
+                                });
+                                res.json({status: 'ok'});
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
-
-    var params = {
-        OutputFormat: 'mp3',
-        Text: req.body.message, //'<speak>Hey there Brian, I just love that push you made to Git Hub. Talk with you later <break time="250ms"/>  tootles!</speak>', //req.body.text,
-        VoiceId: 'Salli',
-        TextType: (req.body.message.indexOf('<speak>') !== -1) ? 'ssml' : 'text'
-    };
-
-    polly.synthesizeSpeech(params, function (err, data) {
-        if (err) {
-            console.log('synthesizeSpeech err', err);
-            res.json(err);
-        } else {
-            console.log('synthesizeSpeech ok');
-            var ts = (new Date).getTime();
-            var bucketName = process.env.BUCKETNAME;
-            var s3Params = {
-                Bucket: bucketName,
-                Key: ts + '.mp3',
-                ACL: 'public-read',
-                Body: data.AudioStream,
-                ContentType: 'audio/mpeg',
-                Expires: (ts / 1000) + (120)
-            };
-            s3.putObject(s3Params, function (err, data) {
-                if (err) {
-                    console.log('putObject mp3 err');
-                    res.json(err);
-                } else {
-                    console.log('putObject mp3 ok');
-
-                    s3Params = {
-                        Bucket: bucketName,
-                        Key: ts + '.xml',
-                        ACL: 'public-read',
-                        Body: '<?xml version="1.0" encoding="UTF-8"?>' +
-                        '<Response>' +
-                        '<Play>' + 'https://s3.amazonaws.com/' + bucketName + '/' + ts + '.mp3' + '</Play>' +
-                        '</Response>'
-                        ,
-                        ContentType: 'text/xml',
-                        Expires: (ts / 1000) + (120)
-                    };
-
-                    s3.putObject(s3Params, function (err, data) {
-                        if (err) {
-                            console.log('putObject xml err');
-                            res.json(err);
-                        } else {
-                            console.log('putObject xml ok');
-                            twilio.calls.create({
-                                url: 'https://s3.amazonaws.com/' + bucketName + '/' + ts + '.xml',
-                                to: req.body.to,
-                                from: req.body.from,
-                                method: 'GET'
-                            }, function (err, call) {
-                                if (err) {
-                                    console.log(err);
-                                } else {
-                                    console.log(call.sid);
-                                }
-                            });
-                            res.json({status: 'ok'});
-                        }
-                    });
-                }
-            });
-        }
-    });
 });
 
 module.exports = router;
